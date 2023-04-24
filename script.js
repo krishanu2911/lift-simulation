992ca6c2ea22bc712e82aebb9e36b09a39237c9d
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2023-04-25 01:49:30 +0530

    feat: form logic

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-console.log("hello")
-console.log("hello")
+const floorLiftCountForm = document.getElementById("floor_lift_count_form");
+
+let floorCount = 0;
+let liftCount = 0
+
+floorLiftCountForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    floorCount = document.getElementById("floor_count").value;
+    liftCount = document.getElementById("lift_count").value;
+})
